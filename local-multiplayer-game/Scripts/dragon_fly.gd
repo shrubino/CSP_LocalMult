e1ea3eddc89f38ec991763a3c45d3c6f3c2f4933
clairelincoln98 <clairelincoln98@gmail.com>
2025-12-09 12:52:24 -0500

    animation and junk

--- local-multiplayer-game/Scripts/dragon_fly.gd
+++ local-multiplayer-game/Scripts/dragon_fly.gd
@@ -3,12 +3,14 @@ extends AnimatableBody2D
 @onready var sprite = $AnimatedSprite2D
 @onready var timer = 0.1
 @onready var canflip = true
-
+var startingPoint
 func _ready() -> void:
 	sprite.flip_h = true
+	startingPoint = global_position
+
 func _process(delta: float) -> void:
 	
-	if position.x < 54 or position.x > 139:
+	if position.x < (global_position.x - 55) or position.x > (global_position.x + 55):
 		if canflip == true:
 			canflip = false
 			sprite.flip_h = not sprite.flip_h
